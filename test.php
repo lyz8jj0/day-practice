9c10444432ce8f85683a2e18630c061d4f2810e2
李新宇 <844216765@qq.com>
2018-05-15 09:16:18 +0800

    add info to a

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lixinyu
+ * Date: 2018/5/14
+ * Time: 下午5:41
+ */
+//$raw_post_data = file_get_contents('php://input');
+//$arr = json_decode($raw_post_data,true);
+$arr['username'] = $_POST['username'];
+//echo json_encode($arr['username']);
+echo $arr['username'];
