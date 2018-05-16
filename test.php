4af6fff29fc369c9094c1f2cce008260ab7dcdca
李新宇 <844216765@qq.com>
2018-05-16 14:02:09 +0800

    增加测试文件

--- test.php
+++ test.php
@@ -8,6 +8,7 @@
  */
 //$raw_post_data = file_get_contents('php://input');
 //$arr = json_decode($raw_post_data,true);
-$arr['username'] = $_POST['username'];
+//$arr['username'] = $_POST['username'];
 //echo json_encode($arr['username']);
-echo $arr['username'];
+//echo $arr['username'];
+echo '1111';
